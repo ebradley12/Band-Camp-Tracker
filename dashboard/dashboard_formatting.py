5b3e84ebd1a6315938ebaaddc19603c527758e8d
Gabriel <gabrielnsiah2011@gmail.com>
2024-12-12 17:45:22 +0000

    Refactoring Code

--- dashboard/dashboard_formatting.py
+++ dashboard/dashboard_formatting.py
@@ -5,7 +5,8 @@ import streamlit as st
 
 def customize_sidebar(degree: int, start_hex: str, end_hex: str):
     """
-    Applies custom styling to change the sidebar background color to a gradient and style the sidebar.
+    Applies custom styling to change the sidebar 
+    background color to a gradient and style the sidebar.
     """
 
     st.markdown(
@@ -28,38 +29,9 @@ def customize_sidebar(degree: int, start_hex: str, end_hex: str):
     )
 
 
-def change_radio_button_colours():
-    st.markdown(
-        """
-    <style>
-    /* Sidebar background color */
-    div[data-testid="stSidebar"] > div:first-child {
-        background-color: #f0f2f6;
-    }
-
-    /* Radio button text color */
-    div[data-testid="stSidebar"] .stRadio > label {
-        color: #8c52ff;
-        font-size: 16px;
-    }
-
-    /* Radio button dot color */
-    div[data-testid="stSidebar"] .stRadio > div > div {
-        color: #8c52ff; /* Default color */
-        background-color: #8c52ff; /* Dot fill */
-    }
-
-    /* Hover effect for radio button */
-    div[data-testid="stSidebar"] .stRadio > div > div:hover {
-        color: #5ce1e6;
-        background-color: #5ce1e6;
-    }
-    </style>
-    """,
-        unsafe_allow_html=True,
-    )
-
-
 def glamourize_dashboard():
+    """
+    Runs all functions related to 
+    formatting the Streamlit dashboard
+    """
     customize_sidebar(180, "8c52ff", "5ce1e6")
-    change_radio_button_colours()
